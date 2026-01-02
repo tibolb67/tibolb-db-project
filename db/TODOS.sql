337a5719e99f711aa8d1cb91518ef9ec4de7ef09
tibolb67 <tibo.le@mng.ch>
2026-01-02 12:56:56 +0100

    table done

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -56,9 +56,20 @@ CREATE TABLE kategorie (
 )
 
 CREATE TABLE budget (
-    budget_id
+    budget_id int AUTO_INCREMENT primary key,
+    user_id int not null,
+    kategorie_id int not null,
+    income_id int,
+    limit_budget decimal(10, 2) not null,
+    foreign key(user_id) references users(id),
+    foreign key (kategorie_id) references kategorien(kategorie_id),
+    foreign key (income_id) references income(income_id)
 )
 
 CREATE TABLE income (
-    income_id
-)
+    income_id int AUTO_INCREMENT primary key,
+    user_id int not null,
+    betrag decimal(10, 2) not null,
+    datum datetime not null,
+    foreign key(user_id) references users(id)
+);
